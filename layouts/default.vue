eeb03ecbf5e7177c90a71bee5115977e92de2c87
Garrett <garrettsoumon@gmail.com>
2018-10-01 22:52:09 -0500

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 50px;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Skills msg=""/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Skills from "@/components/Skills.vue";
+
+export default {
+  name: "home",
+  components: {
+    Skills
+  }
+};
+</script>
+
+<style>
+
+</style>
